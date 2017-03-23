7f988fea20e9d7d779bee60b16df77e54124611f
Nestor Solis <nsolis@researchmobile.co>
2017-03-22 21:21:12 -0600

    vista exportar excel

---
+++ resources/views/reportecobertura.blade.php
@@ -0,0 +1,70 @@
+<?php  
+use App\Http\Requests;
+use App\PuntosVentas;
+use App\Modelos;
+
+$sucursal= Request::input('idsucursal');
+$models= Request::input('modelos');
+
+$registros= Array();
+$modelos= explode(",", $models);
+$nModelos= Array();
+$consulta="puntosventas.nombre as PDV, ";
+for ($i = 0; $i < count($modelos); $i++) {
+    $modelo=$modelos[$i];
+    $ultimo= count($modelos)-1;
+    $consulta.="sum(case when vistaventas.idmodelo=".$modelo." then vistaventas.sellout else 0 end) as sellout_".$modelo.", ";
+    $consulta.="sum(case when vistaventas.idmodelo=".$modelo." then vistaventas.inventory else 0 end) as inventory_".$modelo.", ";
+    $consulta.="ifnull((select cp.cantidad from categoriasplantillas cp inner join plantillas p ON p.idcategoria_plantilla=cp.id where p.idpuntoventa=puntosventas.id and p.idmodelo=".$modelo."),0) as plantilla_".$modelo.", ";
+    $consulta.="0 as xvender_".$modelo.", ";
+    $consulta.="case when vistaventas.idmodelo=".$modelo." then if(vistaventas.inventory>0, if(vistaventas.sellout>0,((vistaventas.sellout/vistaventas.inventory)*7),vistaventas.inventory),0) else 0 end as diasexhibicion_".$modelo.",";
+    if($ultimo==$i)
+        $consulta.="case when vistaventas.idmodelo=".$modelo." then if(vistaventas.inventory>1, if(vistaventas.sellout>0,((vistaventas.sellout/vistaventas.inventory)*7),vistaventas.inventory),0) else 0 end as diasventas_".$modelo;
+    else
+        $consulta.="case when vistaventas.idmodelo=".$modelo." then if(vistaventas.inventory>1, if(vistaventas.sellout>0,((vistaventas.sellout/vistaventas.inventory)*7),vistaventas.inventory),0) else 0 end as diasventas_".$modelo.", ";
+    $tempModel= Modelos::find($modelo);
+    if($tempModel){
+        array_push($nModelos, $tempModel->nombre);
+    }
+}
+$registros = 
+    PuntosVentas::selectRaw($consulta)->leftJoin('vistaventas', function ($join) {
+            $join->on('vistaventas.idpuntoventa', '=', 'puntosventas.id')
+            ->where('vistaventas.semana', '=', Request::input('semana'));
+    })->where('puntosventas.idsucursal', $sucursal)
+    ->groupBy('puntosventas.id')
+    ->get();
+
+
+?>
+
+<table >
+    <thead>
+        <tr>
+            <th>Punto de venta</th>
+            @for ($i = 0; $i < count($modelos); $i++)
+            <th>Sellout {{$nModelos[$i]}}</th>
+            <th>Inventory {{$nModelos[$i]}}</th>
+            <th>Plantilla {{$nModelos[$i]}}</th>
+            <th>Vender {{$nModelos[$i]}}</th>
+            <th>Días Exhibición {{$nModelos[$i]}}</th>
+            <th>Días Ventas {{$nModelos[$i]}}</th>
+            @endfor
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($registros as $item)
+        <tr>
+            <td>{{$item->PDV}}</td>
+             @for ($x = 0; $x < count($modelos); $x++)
+             <td>{{$item->{'sellout_'.$modelos[$x]} }}</td>
+             <td>{{$item->{'inventory_'.$modelos[$x]} }}</td>
+             <td>{{$item->{'plantilla_'.$modelos[$x]} }}</td>
+             <td>{{$item->{'xvender_'.$modelos[$x]} }}</td>
+             <td>{{$item->{'diasexhibicion_'.$modelos[$x]} }}</td>
+             <td>{{$item->{'diasventas_'.$modelos[$x]} }}</td>
+             @endfor
+        </tr>
+        @endforeach
+    </tbody>
+</table>
